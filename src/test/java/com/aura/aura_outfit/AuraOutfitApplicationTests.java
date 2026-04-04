d2c6182715bb8f64c4a60018b8c9bd8a83d96e9b
Maxi Vicente <maximillian100@gmail.com>
2026-04-03 23:30:07 -0300

    Projeto inicial Spring Boot

---
+++ src/test/java/com/aura/aura_outfit/AuraOutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aura.aura_outfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraOutfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
